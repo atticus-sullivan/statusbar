630f4686a2c73926e0d6b7db6b0a6afe0f28d6a1
luk <mail-login+gitlab@protonmail.com>
2020-04-10 00:13:41 +0200

    Removed unused foreign scripts

--- mailbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Displays number of unread mail and an loading icon if updating.
-
-unread="$(find ~/.local/share/mail/*/INBOX/new/* -type f | wc -l 2>/dev/null)"
-
-icon="$(cat "/tmp/imapsyncicon_$USER")"
-
-[ "$unread" = "0" ] && [ "$icon" = "" ] || echo "📬 $unread$(cat "/tmp/imapsyncicon_$USER" 2>/dev/null)"
